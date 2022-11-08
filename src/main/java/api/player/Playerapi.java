6408640e1db0d34a0321a78aae1ba4932574af7c
makamys <makamys@outlook.com>
2022-11-08 21:07:19 +0100

    Init project

--- src/main/java/api/player/Playerapi.java
+++ src/main/java/api/player/Playerapi.java
@@ -1,14 +1,14 @@
-package ${package};
+package api.player;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Playerapi.MODID, version = Playerapi.VERSION)
+public class Playerapi
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "PlayerAPI";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
